9fa472b6cf3f263df5f680c27472cf93e407287e
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-02-27 20:43:36 +0100

    Created the Hostbuilder

---
+++ KafkaTests/KafkaTests/Program.cs
@@ -0,0 +1,18 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+
+
+CreateHostBuilder(args).Build().Run();
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, collection) =>
+    {
+        collection.AddHostedService<KafkaProducerHostedService>();
+        collection.AddHostedService<KafkaConsumerHostedService>();
+    });
+
