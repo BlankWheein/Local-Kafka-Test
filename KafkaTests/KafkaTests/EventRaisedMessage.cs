1717cb1456f40f1291fff5e505f2dd03c07bdd94
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-02-27 20:43:07 +0100

    Create the ProtoBuf de/serializer

---
+++ KafkaTests/KafkaTests/EventRaisedMessage.cs
@@ -0,0 +1,19 @@
+﻿using ProtoBuf;
+
+[ProtoContract]
+public class EventRaisedMessage
+{
+    [ProtoMember(1)]
+    public DateTime Date { get; set; }
+    public EventRaisedMessage() {}
+
+    public EventRaisedMessage(DateTime date)
+    {
+        Date = date;
+    }
+    public override string ToString()
+    {
+        return $"{Date}";
+    }
+}
+
---
+++ KafkaTests/KafkaTests/ProtoSerializer.cs
@@ -0,0 +1,30 @@
+﻿using ProtoBuf;
+
+public static class ProtoSerializer
+{
+    public static byte[] GPBSerialization(object objtoSerialize)
+    {
+        if (objtoSerialize == null)
+            return null;
+        try
+        {
+            using MemoryStream stream = new();
+            Serializer.Serialize(stream, objtoSerialize);
+            return stream.ToArray();
+        }
+        catch {
+            throw;
+        }
+    }
+    public static object GPBDeserialization(byte[] data)
+    {
+        if (data == null) return null;
+        try
+        {
+            using MemoryStream stream = new(data);
+            return Serializer.Deserialize(typeof(EventRaisedMessage), stream);
+        }
+         catch { throw; }
+    }
+}
+
