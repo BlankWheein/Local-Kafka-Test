c153666168e866c4486cb90a0c478dac4a3d2501
Blank <47646799+BlankWheein@users.noreply.github.com>
2023-03-26 18:55:52 +0200

    Added support for Avro

---
+++ KafkaTests/KafkaTests/AvroSerializer.cs
@@ -0,0 +1,37 @@
+﻿using SolTechnology.Avro;
+
+public static class AvroSerializer
+{
+    //Supported encoding types:
+        //Null(default)
+        //Deflate
+        //Snappy
+        //GZip
+        //Brotli
+
+    public static byte[] GPBSerialization(object objtoSerialize)
+    {
+        if (objtoSerialize == null)
+            return null;
+        try
+        {
+            byte[] avroObject = AvroConvert.Serialize(objtoSerialize);
+            return avroObject;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+    public static object GPBDeserialization(byte[] data)
+    {
+        if (data == null) return null;
+        try
+        {
+            var result = AvroConvert.Deserialize<EventRaisedMessage>(data);
+            return result;
+        }
+        catch { throw; }
+    }
+}
+
